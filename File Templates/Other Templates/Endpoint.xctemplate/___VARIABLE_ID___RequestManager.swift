ef474edd647118e2b828a6011e3f27b5d9cac41a
andrej.jasso@goodrequest.com <andrejjasso>
2021-09-13 19:44:52 +0200

    fix: Endpoint manager dependency generation

--- File Templates/Other Templates/Endpoint.xctemplate/___VARIABLE_ID___RequestManager.swift
+++ File Templates/Other Templates/Endpoint.xctemplate/___VARIABLE_ID___RequestManager.swift
@@ -25,3 +25,21 @@ class ___VARIABLE_ID___RequestManager: ___VARIABLE_ID___RequestManagerType {
     }
 
 }
+
+// MARK: Manager Implementation
+
+protocol With___VARIABLE_ID___RequestManager: AnyObject {
+
+    var ___VARIABLE_VAR___RequestManager: ___VARIABLE_ID___RequestManagerType { get }
+
+}
+
+// MARK: Manager Implementation
+
+extension DependencyContainer: With___VARIABLE_ID___RequestManager {
+
+    var ___VARIABLE_VAR___RequestManager: ___VARIABLE_ID___RequestManagerType {
+        return ___VARIABLE_ID___RequestManager(server: .base)
+    }
+
+}
